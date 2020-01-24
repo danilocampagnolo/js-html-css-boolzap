a999edf798ca37e3f2848783d2f6ae778ed95d04
Danilo <danilo.campagnolo.93@gmail.com>
2020-01-24 10:10:56 +0100

    input-text

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+$(document).ready(function() {
 
+  $(document).on("click", "i.fa-telegram-plane",
+      function() {
+      var text = $(".write-message input").val();
+      console.log(text);
+      var newMessageInput = $(".template .msg-input").clone();
+      newMessageInput.append(text);
+      $(".chat-window .msg.input").append(newMessageInput);
+    }
+  );
+
+
+
+
+
+});
